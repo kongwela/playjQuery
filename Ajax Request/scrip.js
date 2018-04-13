2c769a6833015157f9f97d4a46dc7b02a0318e0c
kongw <kongwela75@gmail.com>
2018-04-12 23:28:53 -0700

    add ajax code on HTML and JS

---
+++ Ajax Request/scrip.js
@@ -0,0 +1,14 @@
+function loadRepo(){
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+   if(this.readyState == 4&& this.status == 200){
+     results=JSON.parse(this.responseText);
+     for(var i=0;i<results.length;i++){
+         console.log(results[i].name);
+        }  
+    }       
+}
+
+xhttp.open("GET", "https://api.github.com/users/YOURUSERNAME/repos ", true )
+xhttp.send();
+}
